1960964cbf0bd851369a8001b45662aa6609cfdb
rahul8798 <39438361+rahul8798@users.noreply.github.com>
2019-06-08 14:13:08 +0530

    back to original

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
@@ -6,9 +7,9 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).on("click",function() {
-    if (!started) {
-    $("#level-title").text("level " + level);
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
@@ -16,80 +17,63 @@ $(document).on("click",function() {
 
 $(".btn").click(function() {
 
-  var userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
-  // console.log(userClickedPattern);
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-  checkAnswer(userClickedPattern.length - 1);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
 
 function checkAnswer(currentLevel) {
 
-  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
 
-    console.log("success");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
 
-    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
-    if (userClickedPattern.length === gamePattern.length) {
-
-      //5. Call nextSequence() after a 1000 millisecond delay.
-      setTimeout(function() {
-        nextSequence();
-      }, 1000);
-
+      startOver();
     }
-
-  } else {
-
-    console.log("wrong");
-    playSound("wrong");
-    $("body").addClass("game-over");
-    setTimeout(function() {
-      $("body").removeClass("game-over");
-    }, 200);
-
-    $("#level-title").text("Game-Over, click to Restart");
-    startOver();
-  }
-
 }
 
+
 function nextSequence() {
-
   userClickedPattern = [];
-
   level++;
-  $("#level-title").text("level " + level);
-
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColor);
-  animatePress(randomChosenColor);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
-
-}
-
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function() {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
 }
 
 function startOver() {
-
+  level = 0;
   gamePattern = [];
   started = false;
-  level = 0;
-
 }
